888f0034c607f1363fed9a1664bc064fa6065972
Victor Alves <victor.alves.souza007@gmail.com>
2024-04-01 09:47:27 -0300

    Criando scrpit da classificação

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ let num2 =parseInt(document.getElementById('num2').value);
 
 document.getElementById('resultado').innerHTML= num1 * num2;
 }
+
+function inserir(){
+    let numero= document.getElementById("posicao").value-1;
+    let novo=document.getElementById("novo").value;
+    document.getElementsByClassName("time")[numero].innerHTML = novo
+}
